a0b9168bae0d1f6a3faa5fb273f9caa819919f12
Ferreirinha <l.ferreiriha96@gmail.com>
2021-01-16 19:16:56 +0000

    Setup ngrx effects & s. cart effect added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PopUpComponent } from './components/pop-up/pop-up.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,8 +17,10 @@ import { ProductCardComponent } from './components/product-card/product-card.com
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { shoppingCartReducer } from './_store/_reducers/shopping-cart.reducer';
 import { environment } from 'src/environments/environment';
-import { ShoppingCartProductRowComponent } from './shopping-cart-product-row/shopping-cart-product-row.component';
-import { ShoppingCartTableComponent } from './shopping-cart-table/shopping-cart-table.component';
+import { ShoppingCartProductRowComponent } from './components/shopping-cart-product-row/shopping-cart-product-row.component';
+import { ShoppingCartTableComponent } from './components/shopping-cart-table/shopping-cart-table.component';
+import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
+import { shoppingCartEffects } from './_store/_effects/shopping-cart.effects';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shopping-cart', component: ShoppingCartComponent },
@@ -35,12 +38,14 @@ const appRoutes: Routes = [
     SpinnerComponent,
     ShoppingCartProductRowComponent,
     ShoppingCartTableComponent,
+    ShoppingCartSummaryComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     StoreModule.forRoot({ shoppingCart: shoppingCartReducer }),
+    EffectsModule.forRoot([shoppingCartEffects]),
     StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     RouterModule.forRoot(appRoutes),
   ],
