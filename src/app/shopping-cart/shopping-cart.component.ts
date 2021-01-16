7f58b2144314805fb5142a386a55052811c6c485
Ferreirinha <l.ferreiriha96@gmail.com>
2021-01-16 10:31:59 +0000

    set store actions in shopping cart

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -3,7 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Product } from '../Models/product';
 import * as fromShoppingCart from '../_store/_reducers/shopping-cart.reducer';
-
+import * as shoppingCartActions from '../_store/_actions/shopping-cart.actions';
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
@@ -11,9 +11,40 @@ import * as fromShoppingCart from '../_store/_reducers/shopping-cart.reducer';
 })
 export class ShoppingCartComponent implements OnInit {
   products: Observable<{ shoppingCartProducts: Product[] }>;
+
   constructor(private store: Store<fromShoppingCart.AppState>) {}
 
   ngOnInit(): void {
     this.products = this.store.select('shoppingCart');
+  }
+
+  addItem(index: number, product: Product) {
+    this.store.dispatch(
+      new shoppingCartActions.UpdateShoppingCartProduct({
+        index: index,
+        product: product,
+        action: '+',
+      })
+    );
+  }
+
+  removeItem(index: number, product: Product) {
+    if (product.qtt == 1) {
+      this.deleteItem(index);
+    } else {
+      this.store.dispatch(
+        new shoppingCartActions.UpdateShoppingCartProduct({
+          index: index,
+          product: product,
+          action: '-',
+        })
+      );
+    }
+  }
+
+  deleteItem(index: number) {
+    this.store.dispatch(
+      new shoppingCartActions.DeleteShoppingCartProduct(index)
+    );
   }
 }
