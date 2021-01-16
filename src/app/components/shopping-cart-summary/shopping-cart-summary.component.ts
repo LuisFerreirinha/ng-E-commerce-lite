17bfb9905182e147bec759b62943fcbae1e4bdf4
Ferreirinha <l.ferreiriha96@gmail.com>
2021-01-16 20:02:16 +0000

    shopping cart summary layout and checkout improved

--- src/app/components/shopping-cart-summary/shopping-cart-summary.component.ts
+++ src/app/components/shopping-cart-summary/shopping-cart-summary.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { IpopUp } from 'src/app/Models/IPopUp';
+import { PopUpService } from 'src/app/_services/pop-up.service';
+import * as fromShoppingCart from '../../_store/_reducers/shopping-cart.reducer';
 @Component({
   selector: 'app-shopping-cart-summary',
   templateUrl: './shopping-cart-summary.component.html',
-  styleUrls: ['./shopping-cart-summary.component.css']
+  styleUrls: ['./shopping-cart-summary.component.css'],
 })
-export class ShoppingCartSummaryComponent implements OnInit {
-
-  constructor() { }
+export class ShoppingCartSummaryComponent implements OnInit, OnDestroy {
+  totalItems;
+  total;
+  subscription: Subscription;
+  constructor(
+    private store: Store<fromShoppingCart.AppState>,
+    private popupService: PopUpService
+  ) {}
 
   ngOnInit(): void {
+    this.subscription = this.store.select('shoppingCart').subscribe((store) => {
+      this.total = store.Total;
+      this.totalItems = store.TotalItems;
+    });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  chekout() {
+    const popup: IpopUp = {
+      message: `Thank you for shopping with us!`,
+      timer: 3,
+      type: 'info',
+    };
+    this.popupService.addPopup(popup);
+  }
 }
