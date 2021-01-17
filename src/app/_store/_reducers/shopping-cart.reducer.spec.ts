ca390cc142bdc0c3c340e0f30e99f2075c1baf4e
Ferreirinha <l.ferreiriha96@gmail.com>
2021-01-17 17:26:03 +0000

    Tests Reducer tests revision All Ok

--- src/app/_store/_reducers/shopping-cart.reducer.spec.ts
+++ src/app/_store/_reducers/shopping-cart.reducer.spec.ts
@@ -73,4 +73,164 @@ describe('ShoppingCartReducer', () => {
       expect(state).toEqual(_newState);
     });
   });
+
+  describe('Update product', () => {
+    it('should add 1 item and increment total to the respective product', () => {
+      const initialState: fromShoppingCart.State = {
+        shoppingCartProducts: [
+          {
+            product: { name: 'test', id: 1, price: 10, photo: '' },
+            qtt: 1,
+            total: 10,
+          },
+        ],
+        TotalItems: 1,
+        Total: 10,
+      };
+
+      const payload = {
+        index: 0,
+        product: new Product({
+          name: 'test',
+          id: 1,
+          price: 10,
+          photo: '',
+        }),
+        action: '+',
+      };
+
+      const _newState: fromShoppingCart.State = {
+        shoppingCartProducts: [
+          {
+            product: { name: 'test', id: 1, price: 10, photo: '' },
+            qtt: 2,
+            total: 20,
+          },
+        ],
+        TotalItems: 2,
+        Total: 20,
+      };
+
+      const action = new shoppingCartACtions.UpdateShoppingCartProduct(payload);
+
+      const state = fromShoppingCart.shoppingCartReducer(initialState, action);
+
+      expect(state).toEqual(_newState);
+    });
+  });
+
+  describe('Update product', () => {
+    it('should remove 1 item and decrease total to the respective product', () => {
+      const initialState: fromShoppingCart.State = {
+        shoppingCartProducts: [
+          {
+            product: { name: 'test', id: 1, price: 10, photo: '' },
+            qtt: 2,
+            total: 20,
+          },
+        ],
+        TotalItems: 2,
+        Total: 20,
+      };
+
+      const payload = {
+        index: 0,
+        product: {
+          product: { name: 'test', id: 1, price: 10, photo: '' },
+          qtt: 2,
+          total: 20,
+        },
+        action: '-',
+      };
+
+      const _newState: fromShoppingCart.State = {
+        shoppingCartProducts: [
+          {
+            product: { name: 'test', id: 1, price: 10, photo: '' },
+            qtt: 1,
+            total: 10,
+          },
+        ],
+        TotalItems: 1,
+        Total: 10,
+      };
+
+      const action = new shoppingCartACtions.UpdateShoppingCartProduct(payload);
+
+      const state = fromShoppingCart.shoppingCartReducer(initialState, action);
+
+      expect(state).toEqual(_newState);
+    });
+  });
+
+  describe('Delete product', () => {
+    it('should delete the respective product', () => {
+      const initialState: fromShoppingCart.State = {
+        shoppingCartProducts: [
+          {
+            product: { name: 'test', id: 1, price: 10, photo: '' },
+            qtt: 1,
+            total: 10,
+          },
+        ],
+        TotalItems: 1,
+        Total: 10,
+      };
+
+      const payload = 0;
+
+      const _newState: fromShoppingCart.State = {
+        shoppingCartProducts: [],
+        TotalItems: 0,
+        Total: 0,
+      };
+
+      const action = new shoppingCartACtions.DeleteShoppingCartProduct(payload);
+
+      const state = fromShoppingCart.shoppingCartReducer(initialState, action);
+
+      expect(state).toEqual(_newState);
+    });
+  });
+
+  describe('Delete product', () => {
+    it('should load a given state', () => {
+      const initialState: fromShoppingCart.State = {
+        shoppingCartProducts: [
+          {
+            product: { name: 'test', id: 1, price: 10, photo: '' },
+            qtt: 1,
+            total: 10,
+          },
+        ],
+        TotalItems: 1,
+        Total: 10,
+      };
+
+      const _newState: fromShoppingCart.State = {
+        shoppingCartProducts: [
+          {
+            product: { name: 'test', id: 1, price: 10, photo: '' },
+            qtt: 1,
+            total: 10,
+          },
+          {
+            product: { name: 'test', id: 2, price: 30, photo: '' },
+            qtt: 2,
+            total: 60,
+          },
+        ],
+        TotalItems: 3,
+        Total: 70,
+      };
+
+      const action = new shoppingCartACtions.LoadShoppingCartProducts(
+        _newState
+      );
+
+      const state = fromShoppingCart.shoppingCartReducer(initialState, action);
+
+      expect(state).toEqual(_newState);
+    });
+  });
 });
